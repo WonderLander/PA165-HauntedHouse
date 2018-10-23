d6c54f51407eaea0a9d0f8f48bb9c555fde3e71b
x451363 <x451363@fi.muni.cz>
2018-10-23 17:02:02 +0200

    Finished BogeymanDao interface.

--- haunted-house/src/main/java/cz/muni/fi/hauntedhouse/entity/Bogeyman.java
+++ haunted-house/src/main/java/cz/muni/fi/hauntedhouse/entity/Bogeyman.java
@@ -3,6 +3,9 @@ package cz.muni.fi.hauntedhouse.entity;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalTime;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Ondrej Krcma 451363
@@ -13,26 +16,23 @@ public class Bogeyman {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column
     @NotNull
+    @Column(unique = true)
     private String name;
 
-    @Enumerated
     @NotNull
+    @Enumerated
     private BogeymanType type;
 
-    @Column
-    private Ability ability;
+    @ManyToMany
+    private Set<Ability> abilities = new HashSet<>();
 
-    @Column
+    @NotNull
+    @ManyToOne
     private House house;
 
-    @Column
-    //TODO @Temporal(TemporalType.TIME)
     private LocalTime hauntStartTime;
 
-    @Column
-    //TODO @Temporal(TemporalType.TIME)
     private LocalTime hountEndTime;
 
     @Column(length = 2000)
@@ -43,7 +43,78 @@ public class Bogeyman {
 
     public Bogeyman(){}
 
-    // TODO: constructor?
+    public long getId() {
+        return id;
+    }
 
-    // TODO: add compare and hash
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BogeymanType getType() {
+        return type;
+    }
+
+    public void setType(BogeymanType type) {
+        this.type = type;
+    }
+
+    public Set<Ability> getAbilities() {
+        return Collections.unmodifiableSet(abilities);
+    }
+
+    public void addAbility(Ability ability) {
+        abilities.add(ability);
+    }
+
+    public House getHouse() {
+        return house;
+    }
+
+    public void setHouse(House house) {
+        this.house = house;
+    }
+
+    public LocalTime getHauntStartTime() {
+        return hauntStartTime;
+    }
+
+    public void setHauntStartTime(LocalTime hauntStartTime) {
+        this.hauntStartTime = hauntStartTime;
+    }
+
+    public LocalTime getHountEndTime() {
+        return hountEndTime;
+    }
+
+    public void setHountEndTime(LocalTime hountEndTime) {
+        this.hountEndTime = hountEndTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    // TODO: equals() and hashCode()
+
 }
