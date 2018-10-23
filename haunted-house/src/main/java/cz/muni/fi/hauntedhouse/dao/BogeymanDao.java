0c06c50b2f8076a3b7a24bcd261dcd1db5f6a4f5
x451363 <x451363@fi.muni.cz>
2018-10-23 11:59:44 +0200

    Partially implemented Bogeyman entity and DAO.

---
+++ haunted-house/src/main/java/cz/muni/fi/hauntedhouse/dao/BogeymanDao.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.hauntedhouse.dao;
+
+import cz.muni.fi.hauntedhouse.entity.Bogeyman;
+
+/**
+ * Interface for Bogeyman DAO.
+ *
+ * @author Ondrej Krcma 451363
+ */
+public interface BogeymanDao {
+
+}
---
+++ haunted-house/src/main/java/cz/muni/fi/hauntedhouse/entity/BogeymanType.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.hauntedhouse.entity;
+
+/**
+ * @author Ondrej Krcma 451363
+ */
+public enum BogeymanType {
+    GHOST, POLTERGEIST, SPIRIT, BANSHEE, WITCH, SPECTRE, WRAITH, ZOMBIE, LICH, SKELETON
+}
