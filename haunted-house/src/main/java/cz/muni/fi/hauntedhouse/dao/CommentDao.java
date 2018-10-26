b0fc0d77d4f72c8bad0d4ce27afba72f8e8e7963
ondra1150 <ondra11150@gmail.com>
2018-10-26 18:34:27 +0200

    comment dao

--- haunted-house/src/main/java/cz/muni/fi/hauntedhouse/dao/CommentDao.java
+++ haunted-house/src/main/java/cz/muni/fi/hauntedhouse/dao/CommentDao.java
@@ -6,8 +6,24 @@ import java.util.List;
 
 public interface CommentDao
 {
+    /**
+     * Find all comments in database
+     * @return
+     */
     public List<Comment>findAll();
+
+    /**
+     * Find comment with id
+     * @param id: id of a comment
+     * @return instance of comment of null
+     */
     public Comment findById(Long id);
+
+    /**
+     * Find all comments by author
+     * @param author: name of the author
+     * @return list fo comments
+     */
     public List<Comment> findByAuthor(String author);
     public void create(Comment comment);
     public void delete(Comment comment);
