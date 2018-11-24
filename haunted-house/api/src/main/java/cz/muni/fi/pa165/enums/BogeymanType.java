10e84d62ab14e5a5dd2b9fb8d5b3ea5d67a4bf45
okrcma <okrcma@gmail.com>
2018-11-24 15:28:37 +0100

    Bogeyman api initial commit.

---
+++ haunted-house/api/src/main/java/cz/muni/fi/pa165/enums/BogeymanType.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.pa165.enums;
+
+/**
+ * @author Ondrej Krcma 451363
+ */
+public enum BogeymanType {
+    GHOST, POLTERGEIST, SPIRIT, BANSHEE, WITCH, SPECTRE, WRAITH, ZOMBIE, LICH, SKELETON
+}
